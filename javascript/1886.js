db29950d3e44ca6855239afe3c6a7bd40913e9f1
sotoEs <esteban.soto@uao.edu.co>
2023-11-16 22:58:15 -0500

    Se agrega el Proyecto

---
+++ javascript/1886.js
@@ -0,0 +1,149 @@
+var dom = document.getElementById('chart-container');
+var myChart = echarts.init(dom, 'dark', {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+var app = {};
+
+var option;
+
+option = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  legend: {
+    data: [ 'Temperaturas bajo 0º Centígrados', 'Temperaturas sobre 0º Centígrados']
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis: [
+    {
+      type: 'value'
+    }
+  ],
+  yAxis: [
+    {
+      type: 'category',
+      axisTick: {
+        show: false
+      },
+      data: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio','Agosto','Septiembre','Octubre', 'Noviembre', 'Diciembre']
+    }
+  ],
+  series: [
+    
+    {
+      name: 'Temperaturas sobre 0º Centígrados',
+      type: 'bar',
+      stack: 'Total',
+      label: {
+        show: true,
+        position: 'right',
+        formatter: function (params) {
+          return `${params.value}ºC`;
+        }
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [,, , , , , , , , , , ]
+    },
+    {
+      name: 'Temperaturas bajo 0º Centígrados',
+      type: 'bar',
+      stack: 'Total',
+      label: {
+        show: true,
+        position: 'left',
+        formatter: function (params) {
+          return `${params.value}ºC`;
+        }
+      },
+      emphasis: {
+        focus: 'series'
+      },
+    
+      data: [-44,  -51 , -43 , -28 , -25 , -35,  -18  ,-31 , -24 , -28,  -28,  -26]
+    }
+  ]
+};
+
+if (option && typeof option === 'object') {
+  myChart.setOption(option);
+}
+
+window.addEventListener('resize', myChart.resize);
+var chartContainer = document.getElementById('chart-container');
+var screenWidth = window.innerWidth;
+
+chartContainer.style.width = screenWidth * 0.8;
+
+
+
+
+// Crea un array de tonos que corresponda a cada mes
+// Crea un array de tonos que corresponda a cada mes
+const tonos = [-44,  -51 , -43 , -28 , -25 , -35,  -18  ,-31 , -24 , -28,  -28,  -26];
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio','Agosto','Septiembre','Octubre', 'Noviembre', 'Diciembre'];
+
+// Genera los botones dinámicamente
+const box = document.querySelector('.box');
+
+// ... Tu código JavaScript existente ...
+
+for (let i = 0; i < 12; i++) {
+  const boton = document.createElement('button');
+  
+  const nombreMes = meses[i % meses.length];
+
+  boton.innerText = `${nombreMes}`;
+  boton.classList.add('boton-tono');
+
+  // Agrega una imagen como fondo solo para los botones con la clase boton-tono
+  const imagen = document.createElement('div');
+  imagen.classList.add('boton-imagen');
+  imagen.style.backgroundImage = 'url(./cl.png)';  // Reemplaza 'tu-imagen.jpg' con la ruta de tu imagen
+  boton.appendChild(imagen);
+
+  box.appendChild(boton);
+
+  const tono = tonos[i];
+  boton.addEventListener('click', () => reproducirTono(tono));
+}
+
+
+
+
+// Función para reproducir tono usando Tone.js
+function reproducirTono(valor) {
+  const synth = new Tone.Synth().toDestination();
+  // Mapea el valor al rango MIDI
+  const frecuencia = Tone.Frequency(valor + 60, "midi");
+
+  // Reproduce el tono durante al menos 2 segundos
+  synth.triggerAttackRelease(frecuencia, "2n");
+}
+
+
+const dropdownBtn = document.querySelector('.dropdown-btn');
+const dropdownContent = document.querySelector('.dropdown-content');
+
+dropdownBtn.addEventListener('click', () => {
+  dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+});
+
+
+
+
+
+
+
+
+
---
+++ javascript/1978.js
@@ -0,0 +1,148 @@
+var dom = document.getElementById('chart-container');
+var myChart = echarts.init(dom, 'dark', {
+  renderer: 'canvas',
+  useDirtyRect: false
+});
+var app = {};
+
+var option;
+
+option = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'shadow'
+    }
+  },
+  legend: {
+    data: [ 'Temperaturas bajo 0º Centígrados', 'Temperaturas sobre 0º Centígrados']
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  xAxis: [
+    {
+      type: 'value'
+    }
+  ],
+  yAxis: [
+    {
+      type: 'category',
+      axisTick: {
+        show: false
+      },
+      data: ['Enero', 'Febrer', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio','Agosto','Septiembre','Octubre', 'Noviembre', 'Diciembre']
+    }
+  ],
+  series: [
+    
+    {
+      name: 'Temperaturas sobre 0º Centígrados',
+      type: 'bar',
+      stack: 'Total',
+      label: {
+        show: true,
+        position: 'right',
+        formatter: function (params) {
+          return `${params.value}ºC`;
+        }
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [6, 10, 19, 17, 9, , 4, , 6, 3, 14, 8]
+    },
+    {
+      name: 'Temperaturas bajo 0º Centígrados',
+      type: 'bar',
+      stack: 'Total',
+      label: {
+        show: true,
+        position: 'left',
+        formatter: function (params) {
+          return `${params.value}ºC`;
+        }
+      },
+      emphasis: {
+        focus: 'series'
+      },
+      data: [, , , , , -1, , -13, , , , ]
+    }
+  ]
+};
+
+if (option && typeof option === 'object') {
+  myChart.setOption(option);
+}
+
+window.addEventListener('resize', myChart.resize);
+var chartContainer = document.getElementById('chart-container');
+var screenWidth = window.innerWidth;
+
+chartContainer.style.width = screenWidth * 0.8;
+
+
+
+
+// Crea un array de tonos que corresponda a cada mes
+// Crea un array de tonos que corresponda a cada mes
+const tonos = [6, 10, 19, 17, 9, -1, 4, -13, 6, 3, 14, 8];
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio','Agosto','Septiembre','Octubre', 'Noviembre', 'Diciembre'];
+
+// Genera los botones dinámicamente
+const box = document.querySelector('.box');
+
+// ... Tu código JavaScript existente ...
+
+for (let i = 0; i < 12; i++) {
+  const boton = document.createElement('button');
+  
+  const nombreMes = meses[i % meses.length];
+
+  boton.innerText = `${nombreMes}`;
+  boton.classList.add('boton-tono');
+
+  // Agrega una imagen como fondo solo para los botones con la clase boton-tono
+  const imagen = document.createElement('div');
+  imagen.classList.add('boton-imagen');
+  imagen.style.backgroundImage = 'url(./cl.png)';  // Reemplaza 'tu-imagen.jpg' con la ruta de tu imagen
+  boton.appendChild(imagen);
+
+  box.appendChild(boton);
+
+  const tono = tonos[i];
+  boton.addEventListener('click', () => reproducirTono(tono));
+}
+
+
+
+
+// Función para reproducir tono usando Tone.js
+function reproducirTono(valor) {
+  const synth = new Tone.Synth().toDestination();
+  // Mapea el valor al rango MIDI
+  const frecuencia = Tone.Frequency(valor + 60, "midi");
+
+  // Reproduce el tono durante al menos 2 segundos
+  synth.triggerAttackRelease(frecuencia, "2n");
+}
+
+
+const dropdownBtn = document.querySelector('.dropdown-btn');
+const dropdownContent = document.querySelector('.dropdown-content');
+
+dropdownBtn.addEventListener('click', () => {
+  dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+});
+
+
+
+
+
+
+
+// ... Tu código JavaScript existente ...
+
